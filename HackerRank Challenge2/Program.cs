33a47f5b96b0da3b33a0cc17f35c007d028e3aa7
Büşra <ilkaybsr@gmail.com>
2021-09-30 21:01:52 +0300

    HackerRank Challenge2

---
+++ HackerRank Challenge2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HackerRank_Challenge2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double meal_cost = Double.Parse(Console.ReadLine());
+            int tip_percent = Int32.Parse(Console.ReadLine());
+            int tax_percent = Int32.Parse(Console.ReadLine());
+
+            double tip = meal_cost * tip_percent / 100;
+            double tax = meal_cost * tax_percent / 100;
+
+            double total = meal_cost + tip + tax;
+           
+
+            Console.WriteLine(Math.Round(total));
+
+           
+              
+
+            
+
+        }
+    }
+}
