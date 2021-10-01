a21e1a3ef5ffa86f1408df1d827d6c47d1062a1c
Büşra <ilkaybsr@gmail.com>
2021-10-02 01:48:34 +0300

    While-Foreach yapısı

---
+++ While-Foreach/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace While_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            // 1 den başlayarak console dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıran program .
+
+            Console.Write("Lütfen bir sayı giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+
+
+            while (sayac<=sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+
+            Console.WriteLine(toplam/sayi);
+
+            // a'dan 'z' ye kadar olan tüm harfleri console a yazdır
+
+            char character = 'a';
+            while (character < 'z')
+            {
+                Console.WriteLine(character);
+                character++;
+            }
+
+            Console.WriteLine("****Foreach*****");
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
