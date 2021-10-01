f94fe92ca32b55b4d170ff0e6e6c3786132a5fbf
Büşra <ilkaybsr@gmail.com>
2021-10-01 11:18:46 +0300

    Try catch finally ve mantıksal hatalar

---
+++ HackerRank Challenge3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRank_Challenge3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
