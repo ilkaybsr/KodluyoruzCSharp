ef4d0b30133f7cc0a575f7f01ae2fe82f057da41
Büşra <ilkaybsr@gmail.com>
2021-10-02 02:48:28 +0300

    ödevler eklendi

---
+++ odev1.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace odev1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n).
+            //Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin.Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+
+            Console.WriteLine("Pozitif Bir Sayı Giriniz : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(("{0} adet sayı giriniz" ,n);
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}. Sayıyı giriniz: " , i+1);
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                if (sayi %2==0)
+                {
+                    Console.WriteLine("çift" + sayi);
+                }
+            }
+        }
+    }
+}
---
+++ odev1.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace odev1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Pozitif iki sayi giriniz ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} adet sayi giriniz", n);
+
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}.Sayıyı Giriniz;", i + 1);
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                if ((sayi == m) || (sayi % m == 0))
+                {
+                    Console.WriteLine("Tam bölünüyor " + sayi);
+                }
+            }
+        }
+    }
+}
