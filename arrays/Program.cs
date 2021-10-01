3ace39f4df5dc7610341529ec04b29aeb35d06ec
Büşra <ilkaybsr@gmail.com>
2021-10-02 02:08:52 +0300

    Dizilerin döngülerle kullanımı

---
+++ arrays/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dizi tanımlama
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = { "Kedi", "Köpek", "Kuş", "Maymun" };
+
+            int[] dizi;
+            dizi = new int[5];
+
+            //Dizilerde değer atama ve erişim
+            renkler[0] = "Mavi";
+
+            dizi[3] = 10;
+
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            //Döngüle dizi kullanımı
+            //Klavyeden girilen n tane sayının ortalamasını alan program
+
+            Console.Write("Lütfen dizinin eleman sayısını giriniz:" );
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}. sayısı giriniz :", i + 1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+            
+                toplam += sayi;
+                Console.WriteLine("Ortalama :" + toplam/diziUzunlugu);
+            
+
+        }
+    }
+}
