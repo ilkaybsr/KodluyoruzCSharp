a3fa9a8c60310492df415088be45808ccb046b60
Büşra <ilkaybsr@gmail.com>
2021-10-02 01:34:01 +0300

    HackerRank Day5

---
+++ HackerRank Day5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HackerRank_Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine( n + " x " + i + " = " +(n*i));
+            }
+        }
+    }
+}
